fbe848066834ac49ca2645e9f7ca84b9838f3496
Antoine07 <antoine.lucsko@gmail.com>
2020-10-20 13:02:27 +0200

    recursive acc

--- part1_introduction/Exercices/chap1/accumulator_v2.js
+++ part1_introduction/Exercices/chap1/accumulator_v2.js
@@ -1,14 +1,44 @@
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function accumulator_v2(numbers, acc = 0) {
+    // console.log(acc);
 
-    if(numbers.length === 0) return ;
+    if(numbers.length === 0) return acc ;
 
     const number = numbers.shift();
+    //console.log(number);
+    acc += number;
 
-    console.log(number);
-
-    accumulator_v2(numbers);
+    return accumulator_v2(numbers, acc);
 }
 
-accumulator_v2(numbers);
+console.log( accumulator_v2(numbers) ) ;
+
+/*
+numbers = [1, 2]
+accumulator_v2([ 1, 2], acc = 0)
+    return accumulator_v2([2], acc = 1)
+        return accumulator_v2([], acc = 3)
+            return 3
+
+1. return return return 3
+2. return return 3
+3. return 3
+4. 3
+
+
+console.log('resultat final', accumulator_v2(numbers));
+
+// les fonctions sont successivement appelées 
+accumulator_v2([ 1, 2], acc = 0) 
+{
+    return accumulator_v2([ 2], acc = 1){
+
+        return accumulator_v2([], acc = 3 ){
+
+            return 3
+        }
+    }
+}
+
+*/
