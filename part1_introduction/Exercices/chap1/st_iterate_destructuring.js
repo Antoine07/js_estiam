2e32cd4d38718892ea162dc6232c8429475a0461
Antoine07 <antoine.lucsko@gmail.com>
2020-10-27 12:35:56 +0100

    iterate desctructuring

---
+++ part1_introduction/Exercices/chap1/st_iterate_destructuring.js
@@ -0,0 +1,25 @@
+const students = [
+    {
+        name: "Alan",
+        family: {
+            mother: "Isa",
+            father: "Philippe",
+            sister: "Sylvie",
+        },
+        age: 35,
+    },
+    {
+        name: "Bernard",
+        family: {
+            mother: "Particia",
+            father: "Cécile",
+            sister: "Annie",
+        },
+        age: 55,
+    },
+];
+
+for (const { name, family: { sister } } of students) {
+
+    console.log("Name : " + name + " sister : " + sister);
+}
