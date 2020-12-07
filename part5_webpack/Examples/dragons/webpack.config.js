28f96e36b80ac28c492eb6905f468418195835e5
Antoine07 <antoine.lucsko@gmail.com>
2020-12-07 17:26:13 +0100

    dragon webpack

--- part5_webpack/Examples/dragons/webpack.config.js
+++ part5_webpack/Examples/dragons/webpack.config.js
@@ -1,7 +1,18 @@
+const path = require('path');
+
 module.exports = {
+    mode : "development",
+    watch : true,
     entry: './src/app.js', // Point d'entrée
-    // Sortie (fichier de build)
+    // Sortie
     output: {
+        path: path.resolve(__dirname, 'dist'),
         filename: "bundle.js"
+    },
+    // Configuration de webpack-dev-server minimale
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port : 9000,
+        open: true
     }
 }
